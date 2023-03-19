109c5e69135aaf129f1fd38dacba1725c225a803
Josh_Cydeo <dixiemoxie@gmail.com>
2023-03-19 16:09:50 -0400

    commented out tags in my CukesRunner class

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                   "me.jvt.cucumber.report.PrettyReports:target/cucumber"},
         features ="src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        tags = "",
+        //tags = "",
         dryRun = false,
         publish = true
 )
